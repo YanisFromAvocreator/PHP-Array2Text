8e36d84f01c2f75cb5638b239f94bbc63321da1f
YanisFromAvocreator <yanis@avocreator.com>
2017-08-12 23:28:54 +0200

    Create Array2Text.php

---
+++ Array2Text.php
@@ -0,0 +1,37 @@
+<?php
+
+/*************************************************
+
+Made by @YanisOSLM - https://twitter.com/YanisOSLM
+
+**************************************************/
+ 
+function Array2Text( $array ) {
+  
+  /* First, json_encode the array (JSON_UNESCAPED_UNICODE is recommended, cf step 2 ) */
+  $text = json_encode($contenu_alt, JSON_UNESCAPED_UNICODE );
+  
+   /* Then, list noisy tags due to json_encode */
+  $exclusions = array(
+      "[",
+      "]",
+      "{",
+      "}",
+      "\\",
+      ":",
+      '"'
+	);
+  
+  /* We replace all keys */
+  $text = trim(preg_replace('/"[^"]+"\\:/', '', $text));
+  
+  /* We exclude all noisy tags defined in step 2 */
+  foreach( $exclusions as $pat ){
+		$text = trim(str_replace($pat, '', $text));
+	}
+  
+  /* Return the plain text */
+  return $text;
+}
+
+?>
